d1b45734b923bd4abfc1dc33930a8c95780c84a5
Tatiana Noskova <tnoskowa@mail.ru>
2023-06-24 21:11:14 +0500

    fix tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,12 +17,13 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
     @Test
     void testFindMaxMiddle() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long expected = 30;
 
         long actual = service.findMax(incomesInBillions);
 
